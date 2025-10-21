da37a0c8f5c1422147c49b45f2eeb0ace588426a
xavvaaa <hannahnicolecomia16@gmail.com>
2025-10-21 21:18:16 +0800

    moved app.tsx

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import 'react-native-get-random-values';
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppNavigator from './navigation/AppNavigator';
-import { JobProvider } from './contexts/JobContext';
-import { ThemeProvider } from './contexts/ThemeContext';
+import AppNavigator from './src/navigation/AppNavigator';
+import { JobProvider } from './src/contexts/JobContext';
+import { ThemeProvider } from './src/contexts/ThemeContext';
 
 const App = () => (
   <SafeAreaProvider>
